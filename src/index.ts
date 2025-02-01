7f48902c562c7860fd6369b0645e977b87a9a771
RWB0104 <psj2716@gmail.com>
2025-02-01 23:39:32 +0900

    chore: 🔨 [UPDATE] 설정 추가에 따른 인덱스 모듈 반영

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,12 @@
  * @since 2025.01.21 Tue 17:52:54
  */
 
+import pkg from '../package.json';
+
 import baseConfig from './base';
 import importConfig from './import';
+import nextConfig from './next';
+import reactConfig from './react';
 import reactHooksConfig from './react-hooks';
 import sortKeysFixConfig from './sort-keys-fix';
 import stylisticConfig from './stylistic';
@@ -20,20 +24,72 @@ import type { ConfigArray } from 'typescript-eslint';
 
 interface Configs
 {
+	/**
+	 * 기본 설정
+	 */
 	baseConfig: Linter.Config;
+
+	/**
+	 * import 설정
+	 */
 	importConfig: Linter.Config;
+
+	/**
+	 * next 설정
+	 */
+	nextConfig: Linter.Config;
+
+	/**
+	 * react 설정
+	 */
+	reactConfig: Linter.Config;
+
+	/**
+	 * react-hooks 설정
+	 */
 	reactHooksConfig: Linter.Config;
+
+	/**
+	 * sort-keys-fix 설정
+	 */
 	sortKeysFixConfig: Linter.Config;
+
+	/**
+	 * stylistic 설정
+	 */
 	stylisticConfig: Linter.Config;
+
+	/**
+	 * TailwindCSS 설정
+	 */
 	tailwindcssConfig: Linter.Config;
+
+	/**
+	 * react-query 설정
+	 */
 	tanstackConfig: Linter.Config;
+
+	/**
+	 * typescript 설정
+	 */
 	tseslintConfig: ConfigArray[number];
+
+	/**
+	 * unused-import 설정
+	 */
 	unusedImportsConfig: Linter.Config;
 }
 
 interface ItcodeConfig
 {
-	rules?: [];
+	/**
+	 * 버전
+	 */
+	version: string;
+
+	/**
+	 * 설정
+	 */
 	configs: Configs;
 }
 
@@ -41,6 +97,8 @@ const itcodeConfig: ItcodeConfig = {
 	configs: {
 		baseConfig,
 		importConfig,
+		nextConfig,
+		reactConfig,
 		reactHooksConfig,
 		sortKeysFixConfig,
 		stylisticConfig,
@@ -48,7 +106,8 @@ const itcodeConfig: ItcodeConfig = {
 		tanstackConfig,
 		tseslintConfig,
 		unusedImportsConfig
-	}
+	},
+	version: pkg.version
 };
 
 export default itcodeConfig;
