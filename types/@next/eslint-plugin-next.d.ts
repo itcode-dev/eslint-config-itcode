df37b579a1c51177cf549540dff2a54aee535d32
RWB0104 <psj2716@gmail.com>
2025-02-01 23:35:30 +0900

    feat: 🏷️ [FEATURE] eslint-plugin-next 타입 신규 추가

---
+++ types/@next/eslint-plugin-next.d.ts
@@ -0,0 +1,15 @@
+/**
+ * @next/eslint-plugin-next 타입 모듈
+ *
+ * @author RWB
+ * @since 2025.02.01 Sat 22:43:37
+ */
+
+/// <reference types="../" />
+
+declare module '@next/eslint-plugin-next'
+{
+	const plugin: AnonyEslintPlugin;
+
+	export default plugin;
+}
