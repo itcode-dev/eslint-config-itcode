e61dd1de3cf283af080d3a3965384c832ff3ee50
RWB0104-AHA <psj2716@gmail.com>
2025-06-24 17:32:43 +0900

    Revert "delete: 🔥 불필요 파일 제거"
    This reverts commit 773db9469ea9ec7c8dfb8e50878d3fd2bebbd8a5.

---
+++ types/eslint-plugin-sort-keys-fix.d.ts
@@ -0,0 +1,15 @@
+/**
+ * eslint-plugin-sort-keys-fix 타입 모듈
+ *
+ * @author RWB
+ * @since 2025.01.24 Fri 17:01:18
+ */
+
+/// <reference types="." />
+
+declare module 'eslint-plugin-sort-keys-fix'
+{
+	const plugin: AnonyEslintPlugin;
+
+	export default plugin;
+}
